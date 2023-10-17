4a9f0912ad81d1f58ee2f6b68df717e31c14bcc2
Bruno Said Alves <86388642+brsaidas@users.noreply.github.com>
2023-10-16 23:32:08 -0300

    Add files via upload
    Início do projeto do código fonte do Thunder.

---
+++ source-code/thunder-gdae.ino
@@ -0,0 +1,13 @@
+// Subsistema de Eletrônica GDAe - 2023.2
+// Integrantes:
+//  Andre, Bruno, Roberto, Renan e Victor 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
